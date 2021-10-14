9e7ad447891a5d0294d20bddeb28ff679311db23
Ayush Shukla <ayushmw@gmail.com>
2021-10-14 13:32:47 +0530

    testing context

--- core/src/main/java/org/zerobs/BookController.java
+++ core/src/main/java/org/zerobs/BookController.java
@@ -1,7 +1,6 @@
 package org.zerobs;
 
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -9,20 +8,15 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import lombok.extern.slf4j.Slf4j;
-import org.zerobs.entities.Principal;
 
-import javax.inject.Inject;
 import javax.validation.Valid;
 import java.util.UUID;
 
 @Controller
 @Slf4j
 public class BookController {
-    @Inject
-    AwsProxyRequest awsProxyRequest;
-
     @Post("book")
-    public BookSaved save(@Valid @Body Book book) throws JsonProcessingException {
+    public BookSaved save(@Valid @Body Book book, AwsProxyRequest awsProxyRequest) throws JsonProcessingException {
         var objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         log.error(objectMapper.writeValueAsString(awsProxyRequest));
