84c71d3a10ed3d51a9cb08602841877ffe25af59
Ayush Shukla <ayushmw@gmail.com>
2021-10-24 19:31:23 +0530

    got rid of implementation group: 'org.apache.commons', name: 'commons-collections4', version: '4.4'

--- core/src/main/java/org/zerobs/polla/utilities/Utils.java
+++ core/src/main/java/org/zerobs/polla/utilities/Utils.java
@@ -1,8 +1,10 @@
 package org.zerobs.polla.utilities;
 
-import org.apache.commons.collections4.ListUtils;
+
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -12,9 +14,13 @@ public class Utils {
     }
 
     public static <T> List<T> cleanList(List<T> list) {
-        return ListUtils.emptyIfNull(list).stream().filter(item -> {
+        return emptyIfNull(list).stream().filter(item -> {
             if (item instanceof String) return StringUtils.isNotBlank((String) item);
             else return item != null;
         }).collect(toList());
+    }
+
+    public static <T> Collection<T> emptyIfNull(Collection<T> collection) {
+        return collection == null ? Collections.emptyList() : collection;
     }
 }
